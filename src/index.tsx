11a3c212f2fff8629edd3f9353137028a4ff1a39
black996 <rharasis@yahoo.com>
2021-11-06 15:56:35 +0200

    initialized redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,15 @@
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
+import reducers from "./redux/reducers";
 
-ReactDOM.render(<App color="blue" />, document.getElementById("root"));
+const store = createStore(reducers, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App color="blue" />
+  </Provider>,
+  document.getElementById("root")
+);
